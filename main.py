b5379b45c068ee0394b2d104e6d69126fec76cce
Ktoś <43894510+Ktos93@users.noreply.github.com>
2024-07-09 11:24:09 +0200

    hacky way to store data in points

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from mathutils import Vector
 from bpy_extras.io_utils import ImportHelper, ExportHelper
 from bpy.app.handlers import persistent
 
-from .utils import draw_list_with_add_remove, get_new_item_id
+from .utils import draw_list_with_add_remove, get_new_item_id, compute_probe_hash
 
 
 class ParsedData:
@@ -64,13 +64,13 @@ def parse_line(line):
     tokens = line.split()
     if tokens[0] == 'c':
         is_curve = True
-        position = (float(tokens[1]), float(tokens[3]), float(tokens[2]))
-        handle_a = (float(tokens[4]), float(tokens[6]), float(tokens[5]))
-        handle_b = (float(tokens[7]), float(tokens[9]), float(tokens[8]))
+        position = (float(tokens[1]), float(tokens[2]), float(tokens[3]))
+        handle_a = (float(tokens[4]), float(tokens[5]), float(tokens[6]))
+        handle_b = (float(tokens[7]), float(tokens[8]), float(tokens[9]))
         
-        position = (position[0], position[2], position[1])
-        handle_a = (handle_a[0], handle_a[2], handle_a[1])
-        handle_b = (handle_b[0], handle_b[2], handle_b[1])
+        # position = (position[0], position[2], position[1])
+        # handle_a = (handle_a[0], handle_a[2], handle_a[1])
+        # handle_b = (handle_b[0], handle_b[2], handle_b[1])
 
         is_station = tokens[11] == "1"
         is_left_station = tokens[11] == "2"
@@ -79,12 +79,12 @@ def parse_line(line):
         is_tunnel = tokens[11] == "4"
         is_unk = tokens[11] == "32"
 
-        station_name = tokens[12] if is_station else None
+        name = tokens[12] if is_station or is_left_station or is_right_station or is_junction else None
     else:
         is_curve = False
         position = (float(tokens[0]), float(tokens[1]), float(tokens[2]))
         
-        position = (position[0], position[1], position[2])
+        # position = (position[0], position[1], position[2])
         handle_a = position
         handle_b = position
 
@@ -95,9 +95,9 @@ def parse_line(line):
         is_tunnel = tokens[4] == "4"
         is_unk = tokens[4] == "32"
         
-        station_name = tokens[12] if is_station else None
+        name = tokens[5] if is_station or is_left_station or is_right_station or is_junction else None
     
-    return ParsedData(position, handle_a, handle_b, is_curve, is_station, is_left_station, is_right_station, station_name, is_junction, is_tunnel, is_unk)
+    return ParsedData(position, handle_a, handle_b, is_curve, is_station, is_left_station, is_right_station, name, is_junction, is_tunnel, is_unk)
 
 def distance(p1, p2):
     dx = p1[0] - p2[0]
@@ -105,54 +105,54 @@ def distance(p1, p2):
     dz = p1[2] - p2[2]
     return math.sqrt(dx * dx + dy * dy + dz * dz)
 
-def export_to_text(point, distance):
+def export_to_text(point, distance, node_name):
         flags = ParsedData.decode_flags(point.radius)
 
         if flags["is_curve"]:
-            pos = f"{point.co[0]:.5f} {point.co[2]:.5f} {point.co[1]:.5f}"
-            handle_a = f"{point.handle_left[0]:.5f} {point.handle_left[2]:.5f} {point.handle_left[1]:.5f}"
-            handle_b = f"{point.handle_right[0]:.5f} {point.handle_right[2]:.5f} {point.handle_right[1]:.5f}"
+            pos = f"{point.co[0]:.4f} {point.co[1]:.4f} {point.co[2]:.4f}"
+            handle_a = f"{point.handle_left[0]:.4f} {point.handle_left[1]:.4f} {point.handle_left[2]:.4f}"
+            handle_b = f"{point.handle_right[0]:.4f} {point.handle_right[1]:.4f} {point.handle_right[2]:.4f}"
             flag = "0"
             station_text = ""
             if flags["is_station"]:
                 flag = "1" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_left_station"]:
                 flag = "2" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_right_station"]:
                 flag = "6" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_junction"]:
                 flag = "8" 
-                station_text = "junction_name_here"
+                station_text = node_name
             elif flags["is_tunnel"]:
                 flag = "4" 
             elif flags["is_unk"]:
                 flag = "32" 
-            return f"c {pos} {handle_a} {handle_b} {distance:.5f} {flag} {station_text}"
+            return f"c {pos} {handle_a} {handle_b} {distance:.4f} {flag} {station_text}"
         else:
-            pos = f"{point.co[0]:.5f} {point.co[1]:.5f} {point.co[2]:.5f}"
+            pos = f"{point.co[0]:.4f} {point.co[1]:.4f} {point.co[2]:.4f}"
             flag = "0"
             station_text = ""
             if flags["is_station"]:
                 flag = "1" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_left_station"]:
                 flag = "2" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_right_station"]:
                 flag = "6" 
-                station_text = "station_name_here"
+                station_text = node_name
             elif flags["is_junction"]:
                 flag = "8" 
-                station_text = "junction_name_here"
+                station_text = node_name
             elif flags["is_tunnel"]:
                 flag = "4" 
             elif flags["is_unk"]:
                 flag = "32" 
                 
-            return f"{pos} {distance:.5f} {flag} {station_text}"
+            return f"{pos} {distance:.4f} {flag} {station_text}"
 
 
 
@@ -172,8 +172,6 @@ class TRAIN_PT_Tools(bpy.types.Panel):
 
         list_col, _ = draw_list_with_add_remove(layout, "train.addtrack", "train.deletetrack",
                                                 TRAIN_UL_TRACKS_LIST.bl_idname, "", context.scene, "tracks", context.scene, "track_index", rows=3)
-        row = list_col.row()
-
 
         selected_track = get_selected_track(context)
         if not selected_track:
@@ -186,11 +184,27 @@ class TRAIN_PT_Tools(bpy.types.Panel):
             layout.prop(selected_track, prop_name)
 
 
+
+
+
         list_col.operator("train.import")
         list_col.operator("train.export")
+
+        layout.label(text="Nodes | If you move station/junction link to list will break!!!")
+        list_col, _ = draw_list_with_add_remove(layout, "train.addnode", "train.deletenode",
+                                                        TRAIN_UL_NODE_LIST.bl_idname, "", selected_track, "nodes", selected_track, "node_index", rows=3)
+        
+        selected_node = get_selected_node(context)
+        if selected_node:
+            layout.separator()
+            for prop_name in Node_Properties.__annotations__:
+                if prop_name in ["id"]:
+                    continue
+                layout.prop(selected_node, prop_name)
+            
+
         row = list_col.row()
         pie = row.menu_pie()
-  
         pie.operator("train.show", icon="HIDE_OFF")
         pie.operator("train.hide", icon="HIDE_ON")
        
@@ -200,6 +214,11 @@ class TRAIN_PT_Tools(bpy.types.Panel):
 class TRAIN_UL_TRACKS_LIST(bpy.types.UIList):
     bl_idname = "TRAIN_UL_TRACKS_LIST"
     item_icon = "PRESET"
+
+class TRAIN_UL_NODE_LIST(bpy.types.UIList):
+    bl_idname = "TRAIN_UL_NODE_LIST"
+    item_icon = "PRESET"
+
 
 
 class TRAIN_OT_Add_Track(bpy.types.Operator):
@@ -215,6 +234,48 @@ class TRAIN_OT_Add_Track(bpy.types.Operator):
         return {'FINISHED'}
     
 
+class TRAIN_OT_Add_Node(bpy.types.Operator):
+    bl_idname = "train.addnode"
+    bl_label = "Add Node"
+
+    def execute(self, context):
+
+        obj = context.active_object
+        
+        if not obj or obj.type != 'CURVE':
+            return  {"CANCELLED"}
+        
+        spline = obj.data.splines.active
+        if not spline:
+            return  {"CANCELLED"}
+        
+        point = spline.bezier_points[context.scene.curve_point_index]
+        
+        if not point.select_control_point:
+            return {"CANCELLED"}
+        
+        flags = ParsedData.decode_flags(point.radius)
+
+        if not flags["is_station"] and not flags["is_left_station"] and not flags["is_right_station"] and not flags["is_junction"]:
+             return {"CANCELLED"}
+
+
+        track = get_selected_track(context) 
+        nodes = track.nodes
+
+
+        data = {}
+        data[0] = int(point.co[0] * 100.0)& 0xFFFFFFFF
+        data[1] = int(point.co[1] * 100.0) & 0xFFFFFFFF
+        data[2] = int(point.co[2] * 100.0) & 0xFFFFFFFF
+        new_id = compute_probe_hash(data, 0)   
+        item = nodes.add()
+        item.id = new_id
+        item.node_index = context.scene.curve_point_index
+        item.name = f"{('STATION' if flags['is_station'] else 'LEFT STATION' if flags['is_left_station'] else 'RIGHT STATION' if flags['is_right_station'] else 'JUNCTION' if flags['is_junction'] else 'UNKNOWN')} | {item.id}"
+        return {'FINISHED'}
+    
+
 class TRAIN_OT_Delete_Track(bpy.types.Operator):
     bl_idname = "train.deletetrack"
     bl_label = "Delete track"
@@ -227,7 +288,7 @@ class TRAIN_OT_Delete_Track(bpy.types.Operator):
        
         track = get_selected_track(context) 
         object_name = 'Track-' + track.name
-        if bpy.data.objects[object_name]:
+        if object_name in bpy.data.objects:
             bpy.data.objects.remove(bpy.data.objects[object_name], do_unlink=True)
             
         tracks = context.scene.tracks
@@ -235,7 +296,26 @@ class TRAIN_OT_Delete_Track(bpy.types.Operator):
         tracks.remove(track_index)   
         context.space_data.show_gizmo = context.space_data.show_gizmo
         return {'FINISHED'}
-  
+    
+
+
+class TRAIN_OT_Delete_Node(bpy.types.Operator):
+    bl_idname = "train.deletenode"
+    bl_label = "Delete Node"
+
+    @classmethod
+    def poll(cls, context):
+        return get_selected_track(context) is not None
+    
+    def execute(self, context):
+
+        track = get_selected_track(context) 
+        nodes = track.nodes
+        node_index = track.node_index 
+        nodes.remove(node_index)   
+        context.space_data.show_gizmo = context.space_data.show_gizmo
+        return {'FINISHED'}
+    
 
 class TRAIN_OT_Import_Track(bpy.types.Operator, ImportHelper):
     bl_idname = "train.import"
@@ -266,7 +346,7 @@ class TRAIN_OT_Import_Track(bpy.types.Operator, ImportHelper):
                 track = get_selected_track(context)
 
                 object_name = 'Track-' + track.name
-                if bpy.data.objects[object_name]:
+                if object_name in bpy.data.objects:
                     bpy.data.objects.remove(bpy.data.objects[object_name], do_unlink=True)
 
                 track.name = os.path.splitext(os.path.basename(file_path))[0]
@@ -276,8 +356,8 @@ class TRAIN_OT_Import_Track(bpy.types.Operator, ImportHelper):
                 curve_data = bpy.data.curves.new('BezierCurve', type='CURVE')
                 curve_data.dimensions = '3D'    
                 spline = curve_data.splines.new('BEZIER')
-                spline.bezier_points.add(track.curve_points - 1) 
-
+                spline.bezier_points.add(len(parsed_data) - 1 ) 
+                nodes = track.nodes
                 for i, point in enumerate(parsed_data):
                     bp = spline.bezier_points[i]
                     bp.co = Vector(point.position)
@@ -285,7 +365,21 @@ class TRAIN_OT_Import_Track(bpy.types.Operator, ImportHelper):
                     bp.handle_right = Vector(point.handle_b)
                     bp.radius = point.get_combined_flags()  
                     bp.handle_left_type = 'FREE'
-                    bp.handle_right_type = 'FREE'   
+                    bp.handle_right_type = 'FREE' 
+
+                    if point.is_station or point.is_left_station or point.is_right_station or point.is_junction:
+                        data = {}
+                        data[0] = int(bp.co[0] * 100.0) & 0xFFFFFFFF
+                        data[1] = int(bp.co[1] * 100.0) & 0xFFFFFFFF
+                        data[2] = int(bp.co[2] * 100.0) & 0xFFFFFFFF
+                        new_id = compute_probe_hash(data, 0)   
+                        item = nodes.add()
+                        item.id = new_id
+                        item.node_index = i
+                        item.node_name = point.station_name
+                        item.name = f"{('STATION' if point.is_station else 'LEFT STATION' if point.is_left_station else 'RIGHT STATION' if point.is_right_station else 'JUNCTION' if point.is_junction else 'UNKNOWN')} | {point.station_name}"
+
+
 
                 curve_object = bpy.data.objects.new('BezierCurveObject', curve_data)
                 curve_object.name = 'Track-' + track.name
@@ -321,6 +415,7 @@ class TRAIN_OT_Export_Track(bpy.types.Operator, ExportHelper):
             track_type = track.type
             total_points = track.total_points
             curve_points = track.curve_points
+            nodes = track.nodes
 
             # Get curve object data (example: Bezier points)
             curve_object = track.track_object
@@ -334,6 +429,24 @@ class TRAIN_OT_Export_Track(bpy.types.Operator, ExportHelper):
             if curve_data.bezier_points:
                 for i in range(len(curve_data.bezier_points)):
                     point = curve_data.bezier_points[i]
+
+
+                    data = {}
+                    data[0] = int(point.co[0] * 100.0)& 0xFFFFFFFF
+                    data[1] = int(point.co[1] * 100.0) & 0xFFFFFFFF
+                    data[2] = int(point.co[2] * 100.0) & 0xFFFFFFFF
+                    index = compute_probe_hash(data, 0)   
+                  
+                    found_node = None
+                    for node in nodes:
+                        if node.id == index:
+                            found_node = node
+                            break
+                    node_name = ""
+                    if found_node:
+                        node_name = node.node_name
+                    
+
                     if i < len(curve_data.bezier_points) - 1:
                         next_point = curve_data.bezier_points[i + 1]
                         dist_to_next = distance(point.co, next_point.co)
@@ -341,7 +454,7 @@ class TRAIN_OT_Export_Track(bpy.types.Operator, ExportHelper):
                         next_point = curve_data.bezier_points[0]
                         dist_to_next = distance(point.co, next_point.co)
                     
-                    export_data.append(export_to_text(point, dist_to_next))
+                    export_data.append(export_to_text(point, dist_to_next, node_name))
 
             # Write data to file
             with open(file_path, 'w') as file:
@@ -412,7 +525,26 @@ def get_selected_track(context) -> 'Track_Properties':
         return tracks[track_index]
     else:
         return None
+    
+def get_selected_node(context) -> 'Node_Properties':
+    tracks = context.scene.tracks
+    track_index = context.scene.track_index
+    if 0 <= track_index < len(tracks):
+        nodes = tracks[track_index].nodes
+        node_index = tracks[track_index].node_index
+        if 0 <= node_index < len(nodes):
+            return nodes[node_index]
+        else:
+            return None
+    else:
+        return None
 
+class Node_Properties(bpy.types.PropertyGroup):
+
+    name: bpy.props.StringProperty(name="List")
+    node_name: bpy.props.StringProperty(name="Node Name")
+    node_index: bpy.props.IntProperty(name="Node Index")
+    id: bpy.props.StringProperty(name="Id")
 
 class Track_Properties(bpy.types.PropertyGroup):
     name: bpy.props.StringProperty(name="Name")
@@ -425,11 +557,20 @@ class Track_Properties(bpy.types.PropertyGroup):
 
     type: bpy.props.StringProperty(name="Type")
 
+    nodes: bpy.props.CollectionProperty(type=Node_Properties, name="Nodes")
+
+    node_index: bpy.props.IntProperty(name="Curve Points")
+
     track_object: bpy.props.PointerProperty(
         name="Target Object",
         type=bpy.types.Object,
         description="Reference to a target object"
     )
+
+
+
+  
+   
 
 
 class TRAIN_OT_Set_Point_Data(bpy.types.Operator):
@@ -538,7 +679,6 @@ def update_custom_properties(scene, depsgraph):
                     scene.curve_point_index = point_index
 
                     flags = ParsedData.decode_flags(spline.bezier_points[point_index].radius)
-                    print(flags)
                     scene.is_curve = flags['is_curve']
                     scene.is_station = flags['is_station']
                     scene.is_left_station = flags['is_left_station']
@@ -560,8 +700,12 @@ classes = (
     TRAIN_OT_Show,
     TRAIN_OT_Import_Track,
     TRAIN_OT_Export_Track,
+    Node_Properties,
     Track_Properties,
     TRAIN_UL_TRACKS_LIST,
+    TRAIN_UL_NODE_LIST,
+    TRAIN_OT_Add_Node,
+    TRAIN_OT_Delete_Node
 )
 
 def on_update(self, context, flag_name):
@@ -607,9 +751,7 @@ def unregister():
     bpy.app.handlers.depsgraph_update_post.remove(update_custom_properties)
     del bpy.types.Scene.tracks
     del bpy.types.Scene.track_index
-
     del bpy.types.Scene.curve_point_index
-
 
     del bpy.types.Scene.is_curve
     del bpy.types.Scene.is_station
